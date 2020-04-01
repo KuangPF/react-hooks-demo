063b004960fba58ca8177a91e5ce67973da7871c
KuangPF <1633397286@qq.com>
2020-04-01 22:32:31 +0800

    feat: adjust project directory

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: KuangPF
+ * @Date: 2020-04-01 22:14:34
+ * @Description: home page
+ * @LastEditors: KuangPF
+ * @LastEditTime: 2020-04-01 22:27:31
+ */
+
+import React from 'react'
+
+const App = () => {
+  return <div>home</div>
+}
+
+export default App
