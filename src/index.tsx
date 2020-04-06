5e1e96b64527b1137cf81ee4317f2643f2d8b7bf
KuangPF <1633397286@qq.com>
2020-04-06 15:51:55 +0800

    feat: remove React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './routes/index';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './routes/index'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
+// https://github.com/facebook/react/issues/15074
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
 
+ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
