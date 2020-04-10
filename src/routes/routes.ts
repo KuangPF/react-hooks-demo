43937d88fdb1d818c8f71cbb7fdc2f358a496801
KuangPF <1633397286@qq.com>
2020-04-10 21:48:47 +0800

    chore(router): udpate page router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,9 +3,13 @@
  * @Date: 2020-04-01 22:10:38
  * @Description: 路由文件
  * @LastEditors: KuangPF
- * @LastEditTime: 2020-04-05 19:58:24
+ * @LastEditTime: 2020-04-09 20:46:15
  */
 import UseState from 'pages/base/useState'
+import UseReducer from 'pages/base/useReducer'
+import UseRef from 'pages/base/useRef'
+
+import UseProfile from 'pages/custom-hooks-page/useProfile'
 
 const routes = [
   {
@@ -18,6 +22,23 @@ const routes = [
       {
         path: '/base/useState',
         component: UseState
+      },
+      {
+        path: '/base/useReducer',
+        component: UseReducer
+      },
+      {
+        path: '/base/useRef',
+        component: UseRef
+      },
+    ]
+  },
+  {
+    name: 'custom-hooks',
+    children: [
+      {
+        path: '/custom-hooks/useProfile',
+        component: UseProfile
       }
     ]
   }
