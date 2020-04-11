b769f1980d4725c0553c392ce884cbb1395898d5
KuangPF <1633397286@qq.com>
2020-04-11 23:38:04 +0800

    chore(route): add hooks-class route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,7 @@
  * @Date: 2020-04-01 22:10:38
  * @Description: 路由文件
  * @LastEditors: KuangPF
- * @LastEditTime: 2020-04-10 21:50:49
+ * @LastEditTime: 2020-04-11 23:06:28
  */
 import UseState from 'pages/base/useState'
 import UseReducer from 'pages/base/useReducer'
@@ -11,6 +11,9 @@ import UseRef from 'pages/base/useRef'
 
 import UseProfile from 'pages/custom-hooks-page/useProfile'
 import UseInput from 'pages/custom-hooks-page/useInput'
+
+import ClassGreet from 'pages/hooks-class/class-greet'
+import HooksGreat from 'pages/hooks-class/hooks-greet'
 
 const routes = [
   {
@@ -44,6 +47,19 @@ const routes = [
       {
         path: '/custom-hooks/useInput',
         component: UseInput
+      }
+    ]
+  },
+  {
+    name: 'hooks-class',
+    children: [
+      {
+        path: '/hooks-class/class-greet',
+        component: ClassGreet
+      },
+      {
+        path: '/hooks-class/hooks-great',
+        component: HooksGreat
       }
     ]
   }
